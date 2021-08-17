6dc193c0f0c9ddb9e376ca40849c7ed21af65707
Tamajire <wgcampos15@gmail.com>
2021-08-16 19:21:38 -0500

    Enriqueta examen final

--- Examen.cpp
+++ Examen.cpp
@@ -1,9 +1,10 @@
 
-
 #include <stdio.h>
 
 #include <string.h>
-#define MAX_CARACTERES 5
+
+#include <ctype.h>
+#define MAX_CARACTERES 25
 
 //funciones maestra
 
@@ -15,12 +16,20 @@ void imprimir(char*ptrFrase);
 
 int main(){
 
+    //variables
+
     char frase[MAX_CARACTERES];
+    int vocales;
+    //cumplir condiciones 
     do{
         printf("Ingrese la frase, (25 caracteres incluyendo caracter nulo)\n");
         gets(frase);
 
     }while(validar(frase));
+
+    vocales=convertir(frase);
+    
+    printf("\nEl numero total de vocales es %d",vocales);
 
     return 0;
 
@@ -33,7 +42,65 @@ bool validar(char *ptrFrase){
     }
     
     return false;
+}
+
+int convertir(char *ptrFrase){
+    int acumulador=0;
+
+    while(*ptrFrase!='\0'){
+
+        switch (*ptrFrase){
+        
+        case 'A':
+        
+        case 'E':
+
+        case 'I': 
+
+        case 'O': 
+
+        case 'U':
+        acumulador++;
+
+        break;
+
+        case 'a':
+        
+        case 'e':
+
+        case 'i': 
+
+        case 'o': 
+
+        case 'u':
+        *ptrFrase=toupper(*ptrFrase);
+        acumulador++;
+        break;
+         
+        
+        }
+        
+        ptrFrase++;
+        
+    }
+    return acumulador;
 
     
 }
+
+
+void imprimir (char *ptrFrase){
+  int i;
+printf("La frase transformada es: ");
+   for(i=0;i<strlen(ptrFrase);i++){
+        putchar(*(ptrFrase+i));
+    }
+    
+}
+    
+    
+
+
+    
+
 
